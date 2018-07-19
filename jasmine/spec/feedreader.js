91f7ca35249d8c44574ab4a91806f0b83c86b19f
washingtondeveloper <washingtonprogramador@gmail.com>
2018-07-19 19:00:58 -0300

    review

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -28,42 +28,43 @@ $(function() {
         });
 
 
-        /* TODO: Deve escrever um teste que garanta
+        /* Deve escrever um teste que garanta
          * que o URL foi definido e que não esteja vazio.
          */
         it('deve ter url definida em cada feed', function() {
             const urlPattern = new RegExp(/^(https?):\/\/([a-zA-Z0-9_-]+)(\.[a-zA-Z0-9_-]+)+(\/[a-zA-Z0-9_-]+)*\/?$/gi);
 
             allFeeds.forEach(function(feed) {
+                expect(feed.url).toBeDefined();
                 expect(feed.url).toMatch(urlPattern);
-                expect(feed.url).toMatch(/[a-z]+/);
             });
         });
 
 
-        /* TODO: Deve escrever um teste que garanta
+        /* Deve escrever um teste que garanta
          * que o Name foi definido e que não esteja vazio.
          */
         it('deve ter um nome definido em cada feed', function() {
             allFeeds.forEach(function(feed) {
+                expect(feed.name). toBeDefined();
                 expect(feed.name).toMatch(/[a-z]+/i);
             });
         });
     });
 
 
-    /* TODO: Escrever um novo Suite de Teste com nome 'O menu' */
+    /* Escrever um novo Suite de Teste com nome 'O menu' */
     describe('O menu', function() {
 
         /**
-         * TODO: Escrever um teste que vefique que o menu se econtra
+         * Escrever um teste que vefique que o menu se econtra
          * escondido por padrão
          */
         it('deve verificar se o menu esta oculto por padrão', function() {
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
 
-         /** TODO: Escrever um teste que garanta as seguintes mudanças do menu, 
+         /** Escrever um teste que garanta as seguintes mudanças do menu, 
           * quando o ícone do menu é clicado. Esse teste
           * deve ter duas expectativas: o menu exibe quando
           * clicou e esconde quando clicado novamente
@@ -78,21 +79,21 @@ $(function() {
         });
     });
 
-    /* TODO: Escrever um novo suite de teste com nome 'Entradas iniciais' */
+    /* Escrever um novo suite de teste com nome 'Entradas iniciais' */
     describe('Entradas iniciais', function() {
 
         beforeEach(function(done) {
             loadFeed(0, done);
         })   
 
-        /** TODO: Escreva um teste que quando a função loadFeed() for chamada, verificar
+        /**Escreva um teste que quando a função loadFeed() for chamada, verificar
          * se um elemento(com a classe '.entry') esta disponivel dentro do container '.feed'.
          */
         it('deve garantir que a tela seja carregada e verificar se o carregamento de apenas um elemento esta dentro do container', function() {
-            expect($('.feed .entry').hasClass('entry')).toBe(true);
+            expect($('.feed .entry').length).toBeGreaterThan(0);
         })
     });
-    /* TODO: Escreva um novo suite de teste com nome 'Nova seleção de feed' */
+    /* Escreva um novo suite de teste com nome 'Nova seleção de feed' */
     describe('Nova seleção de feed', function() {
 
         let feed1;
@@ -100,18 +101,16 @@ $(function() {
 
         beforeEach(function(done) {
 
-            loadFeed(1, function() {
+            loadFeed(0, function () {
                 feed1 = $('.feed .entry')[0];
-                done();
-            })
+                loadFeed(1, function () {
+                    feed2 = $('.feed .entry')[0];
+                    done();
+                });
+            });
+        });
 
-            loadFeed(0, function() {
-                feed2 = $('.feed .entry')[0];
-                done();
-            })
-        })
-
-        /** TODO: Escreva um teste, para um novo carregamento de um feed.
+        /** Escreva um teste, para um novo carregamento de um feed.
          * quando a função loadFeed() é chamada passamos um parametro para ele.
          * loadFeed(params), quando passamos 0, 1 ... deve ser um novo feed carregado.
          */
